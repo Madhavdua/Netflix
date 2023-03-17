2382bd5c98d19ba6987f562032f68077233fe92d
Madhav Dua <madhavdua26@gmail.com>
2023-03-17 16:13:41 +0530

    update

---
+++ index.js
@@ -0,0 +1,24 @@
+function opened1(){
+    document.getElementById("ans1").classList.toggle("hide");
+    document.getElementById("ans1").classList.toggle("ans");
+};
+function opened2(){
+    document.getElementById("ans2").classList.toggle("hide");
+    document.getElementById("ans2").classList.toggle("ans");
+}
+function opened3(){
+    document.getElementById("ans3").classList.toggle("hide");
+    document.getElementById("ans3").classList.toggle("ans");
+}
+function opened4(){
+    document.getElementById("ans4").classList.toggle("hide");
+    document.getElementById("ans4").classList.toggle("ans");
+}
+function opened5(){
+    document.getElementById("ans5").classList.toggle("hide");
+    document.getElementById("ans5").classList.toggle("ans");
+}
+function opened6(){
+    document.getElementById("ans6").classList.toggle("hide");
+    document.getElementById("ans6").classList.toggle("ans");
+}
